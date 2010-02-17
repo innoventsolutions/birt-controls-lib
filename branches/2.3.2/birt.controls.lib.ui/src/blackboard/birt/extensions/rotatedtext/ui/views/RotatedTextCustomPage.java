cc72d55a9a92d7b777f46db4f7f17aba7e56a3d8
scottr@innoventsolutions.com <scottr@innoventsolutions.com@21256884-09d8-dba0-7f00-83a4341c2b25>
2010-02-17 17:43:43 +0000

    make a full branch.

---
+++ branches/2.3.2/birt.controls.lib.ui/src/blackboard/birt/extensions/rotatedtext/ui/views/RotatedTextCustomPage.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *  Blackboard, Inc. - significant modification / refactor
+ *  Steve Schafer - Innovent Solutions, Inc. - significant modification / refactor
+ *  
+ *******************************************************************************/
+package blackboard.birt.extensions.rotatedtext.ui.views;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+
+/**
+ * RotatedTextCustomPage
+ */
+public class RotatedTextCustomPage extends RotatedTextGeneralPage
+{
+	private Label lbText, lbAngle;
+
+	public void buildUI( Composite parent )
+	{
+		if ( toolkit == null )
+		{
+			toolkit = new FormToolkit( Display.getCurrent( ) );
+			toolkit.setBorderStyle( SWT.NULL );
+		}
+
+		Control[] children = parent.getChildren( );
+
+		if ( children != null && children.length > 0 )
+		{
+			contentpane = (Composite) children[children.length - 1];
+
+			GridLayout layout = new GridLayout( 2, false );
+			layout.marginTop = 8;
+			layout.marginLeft = 8;
+			layout.verticalSpacing = 12;
+			contentpane.setLayout( layout );
+
+			toolkit.createLabel( contentpane, "Text Content:" ); //$NON-NLS-1$
+			lbText = toolkit.createLabel( contentpane, "" ); //$NON-NLS-1$
+			GridData gd = new GridData( );
+			gd.widthHint = 200;
+			lbText.setLayoutData( gd );
+
+			toolkit.createLabel( contentpane, "Rotation Angle:" ); //$NON-NLS-1$
+			lbAngle = toolkit.createLabel( contentpane, "" ); //$NON-NLS-1$
+			gd = new GridData( );
+			gd.widthHint = 200;
+			lbAngle.setLayoutData( gd );
+
+		}
+	}
+
+	protected void updateUI( )
+	{
+		if ( rotatedTextItem != null )
+		{
+			final String text = rotatedTextItem.getText( );
+			lbText.setText( text == null ? "" : text ); //$NON-NLS-1$
+			lbAngle.setText( String.valueOf( rotatedTextItem.getRotationAngle( ) ) );
+		}
+	}
+
+}
---
+++ branches/2.3.2/birt.controls.lib/src/blackboard/birt/extensions/dotbar/DotbarPresentationImpl.java
@@ -0,0 +1,136 @@
+/*
+ * (C) Copyright Blackboard Inc. 1998-2009 - All Rights Reserved
+ *
+ * Permission to use, copy, modify, and distribute this software without prior explicit written approval is
+ * strictly  prohibited. Please refer to the file "copyright.html" for further important copyright and licensing information.
+ *
+ * BLACKBOARD MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.
+ * BLACKBOARD SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
+ * MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ * authors: Scott Rosenbaum / Steve Schafer, Innovent Solutions, Inc.
+ */
+package blackboard.birt.extensions.dotbar;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageOutputStream;
+
+import org.eclipse.birt.core.exception.BirtException;
+import org.eclipse.birt.report.engine.extension.IBaseResultSet;
+import org.eclipse.birt.report.engine.extension.ICubeResultSet;
+import org.eclipse.birt.report.engine.extension.IQueryResultSet;
+import org.eclipse.birt.report.engine.extension.ReportItemPresentationBase;
+import org.eclipse.birt.report.model.api.ExtendedItemHandle;
+import org.eclipse.birt.report.model.api.extension.ExtendedElementException;
+
+/**
+ * Handles the rendering of the control during report generation. This class is specified in the org.eclipse.birt.report.engine.reportitemPresentation
+ * extension.
+ * 
+ * @author Steve Schafer / Innovent Solutions
+ */
+public class DotbarPresentationImpl extends ReportItemPresentationBase
+{
+  private DotbarItem dotbarItem;
+
+  @Override
+  public void setModelObject( ExtendedItemHandle modelHandle )
+  {
+    try
+    {
+      dotbarItem = (DotbarItem) modelHandle.getReportItem();
+    }
+    catch ( ExtendedElementException e )
+    {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public int getOutputType()
+  {
+    return OUTPUT_AS_IMAGE;
+  }
+
+  public Object onRowSets( IBaseResultSet[] results ) throws BirtException
+  {
+    if ( dotbarItem == null )
+      return null;
+
+    DotbarData config = dotbarItem.getConfiguration();
+    int value = 0;
+    if ( results != null && results.length > 0 )
+    {
+      IBaseResultSet baseResultSet = results[ 0 ];
+      if ( baseResultSet instanceof IQueryResultSet )
+      {
+        IQueryResultSet queryResultSet = (IQueryResultSet) baseResultSet;
+        if ( queryResultSet.isBeforeFirst() )
+          queryResultSet.next();
+        Object object = queryResultSet.evaluate( config.valueExpression );
+        value = translateObject( object );
+      }
+      else if ( baseResultSet instanceof ICubeResultSet )
+      {
+        ICubeResultSet cubeResultSet = (ICubeResultSet) baseResultSet;
+        Object object = cubeResultSet.evaluate( config.valueExpression );
+        value = translateObject( object );
+      }
+    }
+    else
+    {
+      Object object = context.evaluate( config.valueExpression );
+      value = translateObject( object );
+    }
+
+    DotbarData.Calculator calculator = config.new Calculator( 1, "in", dpi );
+    BufferedImage bufferedImage = calculator.createSwingImage( value, dotbarItem.getModelHandle() );
+    ByteArrayInputStream bis = null;
+    try
+    {
+      ImageIO.setUseCache( false );
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      ImageOutputStream ios = ImageIO.createImageOutputStream( baos );
+      ImageIO.write( bufferedImage, "png", ios ); //$NON-NLS-1$
+      ios.flush();
+      ios.close();
+      bis = new ByteArrayInputStream( baos.toByteArray() );
+    }
+    catch ( IOException e )
+    {
+      e.printStackTrace();
+    }
+
+    return bis;
+  }
+
+  private int translateObject( Object object )
+  {
+    if ( object instanceof Integer )
+      return ( (Integer) object ).intValue();
+    if ( object instanceof Long )
+      return ( (Long) object ).intValue();
+    if ( object instanceof BigDecimal )
+      return ( (BigDecimal) object ).intValue();
+    if ( object instanceof Double )
+      return ( (Double) object ).intValue();
+    if ( object instanceof String )
+      try
+      {
+        return Integer.parseInt( (String) object );
+      }
+      catch ( NumberFormatException e )
+      {
+        return 0;
+      }
+    return 0;
+  }
+}
